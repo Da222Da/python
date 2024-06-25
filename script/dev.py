2d8c3539b06ab6d6cec52dcad6fd9c79c83b441f
金石流遇上土山焦 <15826653490@163.com>
2024-06-25 21:58:08 +0800

    you-get

--- script/dev.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-result = subprocess.run("jupyter-lab", shell=True, capture_output=True, text=True)
-
-# 检查指令是否成功运行
-if result.returncode == 0:
-    print("运行成功")
-else:
-    print("运行失败")
